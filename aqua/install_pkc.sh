961a274ad7b4e1a9609faa404ce7bef5d4fc73a6
rht <rhtbot@protonmail.com>
2021-08-22 08:55:38 -0400

    Automate updating sidebar and generating sample pages

--- aqua/install_pkc.sh
+++ aqua/install_pkc.sh
@@ -20,4 +20,11 @@ php maintenance/install.php --server="http://localhost:9352" \
                 "Personal Knowledge Container" \
                 "Admin"
 
+# Extend settings
 cat aqua/extraAquaSettings.txt >> LocalSettings.php
+
+# Update sidebar
+php maintenance/edit.php -s "Use PKC sidebar" -u Admin MediaWiki:Sidebar < aqua/sidebar.wiki
+
+# Populate a page
+php maintenance/edit.php -a -u Admin "Moore's Law" < aqua/MooresLaw.wiki
