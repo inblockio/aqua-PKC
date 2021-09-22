b6cf134461dd73120f74393dfd7d9a76dd6e6308
rht <rhtbot@protonmail.com>
2021-09-22 07:54:48 -0400

    Add script to create or promote user

---
+++ scripts/create_and_promote_user.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -e
+
+usage() {
+    cat <<'EOF'
+Usage:
+  create_and_promote_user.sh [--sysop] -u username [-p password]
+
+Create a new user or modifies an existing user for the PKC MediaWiki.
+
+Options:
+  --sysop  Turn the user into an admin
+  -u       Username
+  -p       Password
+EOF
+}
+
+if [ $# -eq 0 ]; then
+    # If no argument is provided, provide info and exit early.
+    usage
+    exit 0
+fi
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+      -h|--help)
+        usage
+        exit 0
+        ;;
+      --sysop)
+        SYSOP=--sysop
+        shift
+        ;;
+      -u)
+        USERNAME="$2"
+        shift
+        shift
+        ;;
+      -p)
+        PASSWORD="$2"
+        shift
+        shift
+        ;;
+      *)    # unknown option
+        usage
+        exit 1
+        ;;
+    esac
+done
+if [ -z "$USERNAME" ]; then
+  echo "Error: '-u <username>' is a required argument."
+  exit 1
+fi
+
+if [ -z "$PASSWORD" ]; then
+  echo "Error: '-p <password>' is a required argument."
+  exit 1
+fi
+
+sudo docker exec -it micro-pkc_mediawiki_1 php maintenance/createAndPromote.php $SYSOP "$USERNAME" "$PASSWORD"
