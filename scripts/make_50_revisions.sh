a50fae97b22565d751db010915121024b753f7a7
fantasticofox <tim.bansemer@posteo.net>
2022-01-09 12:09:36 +0800

    feat: 🎸 Add more random data to page generator

--- scripts/make_50_revisions.sh
+++ scripts/make_50_revisions.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
+generate_random_string() {
+	echo "$(echo $RANDOM | md5sum | head -c 20)"
+}
+
 WALLET_ADDRESS="0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefde"
 page_title="Main Page"
 echo "Adding 50 more revisions to $page_title"
 page_content=$(docker exec micro-pkc-mediawiki php maintenance/getText.php "$page_title")
 for ((i = 1; i <= 50; i++)); do
 	echo "Step $i"
-	echo "$page_content $i" | docker exec -i micro-pkc-mediawiki php maintenance/edit.php -a -u "$WALLET_ADDRESS" "$page_title"
+	page_content="$page_content $i; randomly generated content: $(generate_random_string)<br>"
+	echo "$page_content" | docker exec -i micro-pkc-mediawiki php maintenance/edit.php -a -u "$WALLET_ADDRESS" "$page_title"
 done
