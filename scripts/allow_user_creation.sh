11c7b4736d87e5cab4392ab79430cbd9e1219b56
fantasticofox <tim.bansemer@posteo.net>
2024-04-08 15:37:11 +0200

    Add helper script to modify LocalSettings.php to allow new user creation

---
+++ scripts/allow_user_creation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script needs to be run with sudo."
+  exit 1
+fi
+
+# Place your script commands here
+echo "Running with sufficient privileges."
+
+# Define the path to the file relative to the script location
+FILE_PATH="../mountPoint/backup/LocalSettings.php"
+
+# Check if the file exists
+if [ ! -f "$FILE_PATH" ]; then
+    echo "Error: $FILE_PATH does not exist."
+    exit 1
+fi
+
+# Use sed to modify the file in-place
+sed -i "s/\$wgGroupPermissions\['\*'\]\['createaccount'\] = false;/\$wgGroupPermissions['*']['createaccount'] = true;/g" "$FILE_PATH"
+
+echo "Modified $FILE_PATH to enable account creation."
+
