36e95c3fe29340eb8e1a2482c377e2ff0d44c2d5
rht <rhtbot@protonmail.com>
2024-08-23 07:25:11 -0400

    refactor: Use docker compose instead of docker-compose

--- scripts/enable_mediawiki_gzip_json_compression.sh
+++ scripts/enable_mediawiki_gzip_json_compression.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker exec micro-pkc-mediawiki sed -i '7 i	AddOutputFilterByType DEFLATE application/json' /etc/apache2/mods-enabled/deflate.conf
-docker-compose restart mediawiki
+docker compose restart mediawiki
