26563337f7cc3a4c8556d87d4c53b2b8493dc156
rht <rhtbot@protonmail.com>
2024-08-23 07:25:11 -0400

    refactor: Use docker compose instead of docker-compose

--- scripts/refresh_all_pkc_repos.sh
+++ scripts/refresh_all_pkc_repos.sh
@@ -12,9 +12,9 @@ AQUA_DIR=$(dirname "$AQUA_PKC_DIR")
 # Nuke all
 (cd "$AQUA_PKC_DIR" && ./pkc nuke)
 
-# update aqua-PKC with docker-compose pull
+# update aqua-PKC with docker compose pull
 echo 'This needs to be run in aqua-PKC/scripts' after ./pkc nuke
-(cd "$AQUA_PKC_DIR" && echo 'Checking for updates on aqua-PKC' && git pull && echo 'Checking for new version of containers' && docker-compose pull)
+(cd "$AQUA_PKC_DIR" && echo 'Checking for updates on aqua-PKC' && git pull && echo 'Checking for new version of containers' && docker compose pull)
 (cd "$AQUA_DIR"/DataAccounting && echo 'Checking for updates on DataAccounting' && git pull)
 (cd "$AQUA_DIR"/data-accounting-external-verifier && echo 'Checking for updates on data-accounting-external-verifier' && git pull)
 (cd "$AQUA_DIR"/VerifyPage && echo 'Checking for updates on VerifyPage an rebuilding it' && git pull && npm ci && npm run build)
